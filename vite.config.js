76daf54d824219315659b6a19615f9b4c32f9c3b
Trương Công Hải <quangcaoconghai@gmail.com>
2026-05-20 19:21:55 +0700

    fix: clean replace index and config with absolute relative paths

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', // Chuỗi rỗng giúp tự động nhận diện mọi đường dẫn tĩnh sau khi build
+  base: './',
 })
+
